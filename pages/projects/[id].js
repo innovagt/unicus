95f632abaaa894004b1f2358b606b7ff97826766
geovannygil <geovannygr2018@gmail.com>
2022-02-21 15:33:23 -0600

    Create module project detail

---
+++ pages/projects/[id].js
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout/Layout'
+const ProjectDetail = () => {
+  return (
+    <Layout nav={false}>
+      <h1>Project Singular</h1>
+    </Layout>
+  )
+}
+
+export default ProjectDetail
