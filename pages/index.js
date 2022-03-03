34b5c5d52a9b1c8cbb7787ad21f095ad14636335
geovannygil <geovannygr2018@gmail.com>
2022-03-03 02:42:34 -0600

    API config web add

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Project from "../components/common/Project";
 import Head from "next/head";
 import dynamic from "next/dynamic";
+import ReactMarkdown from "react-markdown";
 import "animate.css";
 import axios from "axios";
 
@@ -18,7 +19,8 @@ const ParticlesWithNoSSR = dynamic(
   { ssr: false }
 );
 
-const Home = ({ projects }) => {
+const Home = ({ projects, configWeb }) => {
+  const { descHome, facebook, instagram, phone, subtitleHome, titleHome, video_director, youtube } = configWeb.data.attributes
   const data_projects = projects.data;
   let count = 0;
 
@@ -43,11 +45,12 @@ const Home = ({ projects }) => {
       <section className="showcase animate__animated animate__fadeIn">
         <div className="overlay">
           <div className="text-showcase">
-            <h1 className="animate__animated animate__fadeInUp">
-              Lorem ipsum <br />
+            <div className="animate__animated animate__fadeInUp">
+              {/* Lorem ipsum <br />
               dolor
-              <span>sit amet</span>
-            </h1>
+              <span>sit amet</span> */}
+              <ReactMarkdown>{titleHome}</ReactMarkdown>
+            </div>
             <Link href="/projects">
               <a className="animate__animated animate__fadeInUp">
                 Ver proyectos
@@ -56,30 +59,34 @@ const Home = ({ projects }) => {
           </div>
           <div className="social-nav">
             <a
-              href="#"
+              href={phone}
               className="animate__animated animate__fadeInRight"
               target="_blank"
+              rel="noreferrer"
             >
               <img src="/img/wt.svg" alt="" />{" "}
             </a>
             <a
-              href="#"
+              href={facebook}
               className="animate__animated animate__fadeInRight"
               target="_blank"
+              rel="noreferrer"
             >
               <img src="/img/f.svg" alt="" />{" "}
             </a>
             <a
-              href="#"
+              href={instagram}
               className="animate__animated animate__fadeInRight"
               target="_blank"
+              rel="noreferrer"
             >
               <img src="/img/i.svg" alt="" />{" "}
             </a>
             <a
-              href="#"
+              href={youtube}
               className="animate__animated animate__fadeInRight"
               target="_blank"
+              rel="noreferrer"
             >
               <img src="/img/yt.svg" alt="" />
             </a>
@@ -96,7 +103,7 @@ const Home = ({ projects }) => {
           autoPlay
         >
           <source
-            src="https://player.vimeo.com/external/198905291.hd.mp4?s=43fc8816fb9ba83fe4e9cbea704645b5b909ea53&amp;profile_id=175"
+            src={!video_director ? "https://player.vimeo.com/external/198905291.hd.mp4?s=43fc8816fb9ba83fe4e9cbea704645b5b909ea53&amp;profile_id=175" : video_director}
             type="video/mp4"
           />
         </video>
@@ -106,12 +113,10 @@ const Home = ({ projects }) => {
         </div>
         <div className="about-text">
           <div className="container-in">
-            <h2 className="wow fadeInUp">THIS IS UNICUS</h2>
-            <p className="wow fadeInUp">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              fringilla sodales bibendum. Integer volutpat magna hendrerit Lorem
-              ipsum dolor si dignissim.
-            </p>
+            <h2 className="wow fadeInUp">{subtitleHome}</h2>
+            <div className="wow fadeInUp">
+              <ReactMarkdown>{descHome}</ReactMarkdown>
+            </div>
             <Link href="/weare">
               <a className="wow fadeInUp">WE ARE</a>
             </Link>
@@ -152,10 +157,13 @@ export const getServerSideProps = async (context) => {
       `${API_URL}/api/projects?sort=date_event:DESC&pagination[start]=0&pagination[limit]=5&fields[0]=title&populate=cover`
     );
 
+    const { data: configWeb } = await axios.get(`${API_URL}/api/configuration`);
+
     // console.log(projects);
     return {
       props: {
         projects,
+        configWeb
       },
     };
   } catch (error) {
